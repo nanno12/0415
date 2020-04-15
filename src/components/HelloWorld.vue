e1acd18d4fe379c8cc640d926a408a4b820ea6cd
unknown <w_nn@winning.com.cn>
2020-04-15 19:05:52 +0800

    初始化成功

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <w-button>123</w-button>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import WinDesign from 'win-design'
+import 'win-design/lib/themes/default.css' // 默认主题样式文件
+import 'win-design/lib/themes/fonts/iconx.js' // 如果你需要使用行业图标
 
+Vue.use(WinDesign)
 Vue.config.productionTip = false
 
 new Vue({
